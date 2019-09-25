67c75522191d8021456d464e12353a40aa6d105c
Ana Laura Feltrim <46962757875@info.local>
2019-09-25 11:19:55 -0300

    mais exercícios

---
+++ matriz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vetor = {1,2,3,4,5,6};
+            System.Console.WriteLine(vetor);
+
+            for(int i=0; i<7; i++){
+                System.Console.WriteLine(vetor[i]);
+            }
+
+            
+
+            
+
+            
+
+            
+
+            
+
+        }
+    }
+}
