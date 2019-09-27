c9a5e2404ec72359acdfd31b8cf74ec1c695cdf0
Ana Laura Feltrim <46962757875@info.local>
2019-09-27 11:16:45 -0300

    orientação ao objeto

---
+++ mcBonalds/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mcBonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente("Alexandre","55 11 9757875785","analaurafeltrim@gmail.com");
+            Console.WriteLine("Nome: " + cliente1.Nome);
+            System.Console.WriteLine("Telefone: " + cliente1.Telefone);
+            System.Console.WriteLine("Email: " + cliente1.Email);
+
+        }
+        
+            
+        
+    }
+}
