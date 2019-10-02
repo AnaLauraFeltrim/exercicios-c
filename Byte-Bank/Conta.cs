c62f3dea3978e848caa0cc86a4db691da0451b55
Ana Laura Feltrim <46962757875@info.local>
2019-10-02 11:27:25 -0300

    sla mano

--- Byte-Bank/Conta.cs
+++ Byte-Bank/Conta.cs
@@ -6,5 +6,19 @@ namespace Byte_Bank
         public string Nome {get; set;}
         public string Email {get; set;}
         public string Senha {get; set;}
+
+        public Conta(string Cpf, string Nome, string Email){
+            this.Nome = Nome;
+            this.Cpf = int.Parse(Cpf);
+            this.Email = Email; 
+        }
+
+        public bool TrocaSenha(string senha){
+            if((senha.Length> 6) && (senha.Length < 16)){
+                return true; 
+            } else {
+                return false;
+            }
+        }
     }
 }
