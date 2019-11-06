236c6e57ea5f4508741222917ef1215efba6640b
Ana Laura Feltrim <46962757875@info.local>
2019-11-06 12:12:11 -0200

    Mc Bonalds pt 1 c# thats it

--- Par ou impar/Program.cs
+++ Par ou impar/Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
 
-namespace Par_ou_impar
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
+namespace Par_ou_impar {
+    class Program {
+        static void Main (string[] args) {
+
             int num1 = 0;
 
-            do{
-            
+            List<int> listaPar = new List<int> ();
+            List<int> listaImpar = new List<int> ();
 
+            do {
 
-            Console.Write("Insira o número ou 0 para terminar: ");
-            num1 = int.Parse(Console.ReadLine());
+                Console.Write ("Insira o número ou 0 para terminar: ");
+                num1 = int.Parse (Console.ReadLine ());
 
-            if(num1 % 2 == 0){
-                Console.WriteLine("O número é par");
-            }
+                if (num1 % 2 == 0) {
+                    listaPar.Add ();
+                    foreach (var item in listaPar) {
+                        Console.WriteLine ("O número é par");
+                        System.Console.WriteLine(item);
+                    }
 
-            else{
-                Console.WriteLine("O número é impar");
-            }
+                } else {
+                    foreach(var item in listaImpar){
+                        Console.WriteLine ("O número é impar");
+                        System.Console.WriteLine(item);
+                    }
+                    
+                }
 
-            }while(num1 != 0);
+            } while (num1 != 0);
 
         }
     }
